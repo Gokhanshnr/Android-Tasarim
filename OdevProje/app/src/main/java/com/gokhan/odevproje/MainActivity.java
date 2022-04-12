5d0f83f81fc99af53cad50393abac902582d4496
Gökhan Sahiner <gokhansahiner98@gmail.com>
2022-04-12 18:53:19 +0300

    Android Nike Tasarım

---
+++ OdevProje/app/src/main/java/com/gokhan/odevproje/MainActivity.java
@@ -0,0 +1,21 @@
+package com.gokhan.odevproje;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+
+}
